54600d10e316946721c8854a213780cd5c4c8be3
linux-wang <wsw1990wsw@gmail.com>
2016-11-04 21:28:06 +0800

    添加一个注释

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ sys.setdefaultencoding('utf-8')
 if __name__ == '__main__':
     start_url = 'http://m.sohu.com'
     begin = datetime.datetime.now()
-    multi_process(start_url, 8)
+    multi_process(start_url, 8)  # 此处参数可以调节线程数
     # process_url(start_url)
     end = datetime.datetime.now()
     print u"用时: ", end - begin
